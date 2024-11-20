6fffa364c905a07c6cb476ae256ad46d3d63f6a3
Ростислав <rostikkodash@gmail.com>
2024-11-20 16:28:49 +0200

    TeleportPlugin

---
+++ src/main/java/me/lodaris/teleportplugin/TeleportPlugin.java
@@ -0,0 +1,17 @@
+package me.lodaris.teleportplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TeleportPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
